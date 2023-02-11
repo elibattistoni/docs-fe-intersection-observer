97994376c741b45c088b85e0a5519a06008b8afd
Elisa Battistoni <eli.battistoni@gmail.com>
2023-02-11 07:53:01 +0100

    Add scroll to top of window

--- 00-intersection-observer-api/03-react-intersectionObserver-02/src/pages/IOPage.js
+++ 00-intersection-observer-api/03-react-intersectionObserver-02/src/pages/IOPage.js
@@ -5,11 +5,15 @@ import classes from "./IOPage.module.css";
 function IOPage(props) {
   const containerRef = useRef();
 
-  function smoothScrollHandler() {
+  function scrollToContainerHandler() {
     containerRef.current.scrollIntoView({
       behavior: "smooth",
       block: "center",
     });
+  }
+
+  function scrollToTitleHandler() {
+    window.scrollTo({ top: 0, behavior: "smooth" });
   }
 
   return (
@@ -17,11 +21,14 @@ function IOPage(props) {
       <h2>{props.title}</h2>
       <h3 className={classes.instructions}>
         Scroll down to see the effect! or click{" "}
-        <span onClick={smoothScrollHandler}>here</span>
+        <span onClick={scrollToContainerHandler}>here</span>
       </h3>
       <div className={classes.empty}></div>
       <h3 ref={containerRef}>{props.subtitle}</h3>
       <div className={classes.cardsContainer}>{props.children}</div>
+      <footer className={classes.footer}>
+        <button onClick={scrollToTitleHandler}>&uarr;</button>
+      </footer>
     </>
   );
 }
